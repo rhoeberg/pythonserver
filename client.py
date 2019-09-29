7897ab3c9fb040a13625e99703d80fcf1ef17a18
rhoeberg <contact@rhoeberg.com>
2019-09-29 13:02:26 +0200

    new extensions 1

--- client.py
+++ client.py
@@ -1,10 +1,23 @@
+import time
+import threading
 import socket
+import configparser
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+autoSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_address = ('localhost', 10001)
+autoSocket.bind(client_address)
 hostname = socket.gethostname()
 ip = socket.gethostbyname(hostname)
 server_address = ('localhost', 10000)
-i = 0
+conCount = 0
+
+connected = False
+conAddr = None
+
+parser = configparser.RawConfigParser()
+parser.read("./opt.conf")
+keepalive = parser.getboolean('client', 'keepalive')
 
 def printByte(data):
     print(data.decode("utf-8"))
@@ -12,6 +25,9 @@ def printByte(data):
 def sendToAddr(msg, addr):
     msg = msg.encode("utf-8")
     sent = sock.sendto(msg, addr)
+
+def sendToCon(msg):
+    sendToAddr(msg, conAddr)
 
 def verifySynAck(msg, addr):
     print(msg)
@@ -28,22 +44,73 @@ def syn():
     success = verifySynAck(data.decode("utf-8"), addr)
     printByte(data)
     sendToAddr("com-0 accept", server_address)
+    global conAddr
+    global connected
+    conAddr = addr
+    connected = True
     return success
 
-def sendMsg(i):
+def sendInputMsg(i):
     msg = "msg-" + str(i) + " " + input()
-    sendToAddr(msg, server_address)
+    if connected != False:
+        sendToAddr(msg, server_address)
+
+def sendAutoMsg(i, msg):
+    msg = "msg-" + str(i) + " " + msg
+    if connected != False:
+        sendToAddr(msg, server_address)
 
 def recMsg():
+    global conCount
     data, addr = sock.recvfrom(4096)
-    i = int(data.decode("utf-8").split(' ')[0][4:]) + 1
+    conCount = int(data.decode("utf-8").split(' ')[0][4:]) + 1
     printByte(data)
-    return i, addr
+    return conCount, addr
 
-while True:
-    success = syn()
-    if success:
-        while True:
-            sendMsg(i)
+def listenForReset():
+    global connected
+    while True:
+        data, addr = autoSocket.recvfrom(4096)
+        if data.decode("utf-8") == "con-res 0xFE":
+            printByte(data)
+            sendToCon("con-res 0xFF")
+            connected = False
+            conAddr = None
+            break
+        
+def heartbeat():
+    while True:
+        time.sleep(3.0)
+        sendToCon("con-h 0x00")
+        
+def sendAuto():
+    threading.Thread(target=listenForReset).start()
+    while True:
+        if connected == True:
+            sendAutoMsg(conCount, "spam")
             i, addr = recMsg()
-    
+        else:
+            break
+        
+def chat():
+    threading.Thread(target=listenForReset).start()
+    if keepalive == True:
+        threading.Thread(target=heartbeat).start()
+        while True:
+            sendInputMsg(conCount)
+            if connected == False:
+                break
+            i, addr = recMsg()
+        
+def main():
+    global conCount
+    while True:
+        success = syn()
+        if success:
+            if auto:
+                sendAuto()
+            else:
+                chat()
+                
+main()
+
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 hostname = socket.gethostname()
@@ -6,6 +7,8 @@ ip = socket.gethostbyname(hostname)
 server_address = ('localhost', 10000)
 print('starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
+connected = False
+conAddr = None
 
 def printByte(data):
     print(data.decode("utf-8"))
@@ -15,25 +18,56 @@ def sendToAddr(msg, addr):
     sent = sock.sendto(msg, addr)
 
 def waitForSyn():
+    sock.setblocking(True)
     data, addr = sock.recvfrom(4096)
     printByte(data)
     sendToAddr("com-0 accept " + str(server_address[0]), addr)
     print("waiting for ack")
     data, addr = sock.recvfrom(4096)
+    global conAddr
+    conAddr = addr
+    print("printing addr and conAddr")
+    print(addr)
+    print(conAddr)
+    connected = True
     printByte(data)
 
 def recMsg():
     data, addr = sock.recvfrom(4096)
-    i = int(data.decode("utf-8").split(' ')[0][4:]) + 1
+    i = 0
+    if data.decode("utf-8") == "con-h 0x00":
+        i = -1
+    else:
+        i = int(data.decode("utf-8").split(' ')[0][4:]) + 1
     printByte(data)
     return i, addr
 
 def sendRes(i, addr):
     sendToAddr("res-" + str(i) + " I am server", addr)
     
+def resetCon():
+    global conAddr
+    
+    resetAddr = ( conAddr[0], 10001 )
+    sendToAddr("con-res 0xFE", resetAddr)
+    conAddr = ""
+    connected = False
+
+def connected():
+    global conAddr
+    sock.settimeout(4.0)
+    while True:
+        try:
+            i, addr = recMsg()
+            if addr[0] == conAddr[0] and i != -1:
+                sendRes(i, conAddr)
+        except socket.timeout as e:
+            print("client timed out")
+            resetCon()
+            break
+    
 while True:
     waitForSyn()
-    while True:
-        i, addr = recMsg()
-        sendRes(i, addr)
+    connected()
+    
     
